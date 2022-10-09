0dc283baad2682a19d4d2493ea388076ede0faac
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-10-09 00:41:13 -0300

    Adicionando exercicios com File

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,71 +1,108 @@
 import pessoa.Pessoa;
+import signo.Signo;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class app {
-    public static void main(String[] args) {
+    private static String HOME = System.getProperty("user.home");
+    private static String HOME_DIR = System.getProperty("user.dir");
 
-//        LocalDateTime localDateMatheus = LocalDateTime.of(1991,9,19,19,22);
+    public static void main(String[] args) throws IOException {
 
-//        mapaAstral(localDateMatheus,"America/Sao_Paulo");
-
-        Pessoa p1 = new Pessoa("Matheus", LocalDateTime.of(1975, 9, 19, 20, 20), "America/Sao_Paulo");
-        Pessoa p2 = new Pessoa("Lucas", LocalDateTime.of(2010, 5, 19, 20, 20), "America/Sao_Paulo");
-        Pessoa p3 = new Pessoa("Fernando", LocalDateTime.of(1990, 9, 10, 20, 20), "America/Sao_Paulo");
-        Pessoa p4 = new Pessoa("Julia", LocalDateTime.of(2012, 8, 10, 20, 20), "America/Sao_Paulo");
-        Pessoa p5 = new Pessoa("Mariana", LocalDateTime.of(2007, 9, 10, 20, 20), "America/Sao_Paulo");
+        System.out.println(HOME);
+        System.out.println(HOME_DIR);
+        String mapas = HOME_DIR.concat("/mapas");
+        String caminhoMapa = HOME_DIR.concat("/mapas/mapa.txt");
+        final File file = new File(caminhoMapa);
+        final Path path = Paths.get(caminhoMapa);
+        final Path mapaAstral = Path.of(mapas, "pessoas.txt");
 
         List<Pessoa> pessoaList = new ArrayList<>();
-        pessoaList.add(p1);
-        pessoaList.add(p2);
-        pessoaList.add(p3);
-        pessoaList.add(p4);
-        pessoaList.add(p5);
 
-        //Filtro de pessoas do signo Virgem e que sao maiores de 18 anos
-        List<Pessoa> ListaDeSinosVirgemMaiorDe18 = pessoaList.stream()
-                .filter(pessoa -> pessoa.getSigno().toString().equals("Virgem"))
-                .filter(pessoa -> pessoa.getIdade() > 18)
-                .collect(Collectors.toList());
+        List<String> readLines = readLines(mapaAstral);
+        readLines.forEach(s -> {
+            List<String> list = Arrays.asList(s.split(";"));
+            pessoaList
+                    .add(
+                            new Pessoa(list.get(0)
+                                    , LocalDateTime.parse(list.get(2), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))
+                                    , list.get(1)));
 
-//        ListaDeSinosVirgemMaiorDe18.forEach(System.out::println);
+        });
 
-        //Filtro de pessoas menores de 18
-        List<Pessoa> menorDe18 = pessoaList.stream().
-                filter(pessoa -> pessoa.getIdade() < 18)
-                .collect(Collectors.toList());
+        pessoaList.forEach( pessoa -> {
 
-        //Filtro de pessoas da geracao Y
-        List<Pessoa> geracaoZ = pessoaList.stream()
-                .filter(pessoa -> pessoa.getGeracao().toString().equals("Y"))
-                .collect(Collectors.toList());
+            String nome = pessoa.getNome();
+            Integer idade = pessoa.getIdade();
+            String geracao = pessoa.getGeracao();
+            Signo signo = pessoa.getSigno();
+            String cidadeNascimento = pessoa.getCidadeNascimento();
+            String dataNascimento = pessoa.getDataNascimento();
 
-//        geracaoZ.forEach(System.out::println);
+            Path caminhoPessoa = Path.of(HOME_DIR,"mapas", nome.replaceAll(" ","-")+".txt");
+            StringBuilder sb = new StringBuilder();
+            String informacoes = sb.append("Nome: ")
+                    .append(nome)
+                    .append("\nCidade de nascimento: ")
+                    .append(cidadeNascimento)
+                    .append("\nData de nascimento: ")
+                    .append(dataNascimento)
+                    .append("\nSigno: ")
+                    .append(signo)
+                    .append("\nIdade: ")
+                    .append(idade)
+                    .append("\nGeracao: ")
+                    .append(geracao)
+                    .append("\n---------------------\n")
+                    .toString();
 
-        //Metodo para para trazer todas as pessoas e as idades delas na proxima copa
-        List<String> pessoasEIdades = pessoaList.stream().map(pessoa -> {
-            return "Nome: " + pessoa.getNome() + " terá " + (pessoa.getIdade() + 4) + " anos na proxima Copa";
-        }).collect(Collectors.toList());
+            write(caminhoPessoa, informacoes);
 
-//        pessoasEIdades.forEach(System.out::println);
+        });
+    }
 
-        //Filtro para obter a pessoa mais velha ordenando e pegando o ultimo item da lista
-        List<Pessoa> collect = pessoaList.stream().sorted(Comparator.comparing(Pessoa::getIdade)).collect(Collectors.toList());
-//        System.out.println(collect.get(collect.size() - 1));
+    static void write(Path path, String participants) {
+        try {
+            Files.write(path, participants.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-        //Filtro para obter a maior e menor idade
-        int menorIdade = pessoaList.stream().mapToInt(Pessoa::getIdade).min().orElse(-1);
-        int maiorIdade = pessoaList.stream().mapToInt(Pessoa::getIdade).max().orElse(-1);
+    static List<String> readLines(Path path) {
+        Stream<String> lines;
+        try {
+            lines = Files.lines(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        List<String> list = lines.collect(Collectors.toList());
+        lines.close();
 
-//        System.out.println(menorIdade);
-//        System.out.println(maiorIdade);
-
-        //Filtro para calcular media das idades
-        double mediaIdade = pessoaList.stream().mapToInt(Pessoa::getIdade).average().orElse(-1);
-        System.out.println(mediaIdade);
+        return list;
     }
 }
+
+
+// Criar arqujivo com Files
+//        Path caminhoASerCriado = Paths.get(mapas, System.currentTimeMillis() + ".txt");
+//        Files.createFile(caminhoASerCriado);
+
+
+// Ler arquivo com File
+//    Scanner sc = new Scanner(file);
+//        while (sc.hasNext()) {
+//                System.out.println(sc.nextLine());
+//                }
